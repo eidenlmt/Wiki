b0ff14f3b67ce7671bbe7103dab1f8610bba3b60
eidenlmt <eidenlouis.mt@gmail.com>
2022-12-06 19:05:46 +0100

    Entry Page Done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/<str:title>", views.content, name="content"),
     path("wiki/<str:title>", views.title, name="title")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from . import util
 
 
@@ -8,7 +7,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def title(request, title):
+def content(request, title):
     return render(request, "encyclopedia/entry.html", {
         "title": util.get_entry(title)
+    })
+
+def title(request, title):
+    f = open(f"entries/{title}.md", "r")
+    return render(request, "encyclopedia/entry.html", {
+        "htmltitle": f.readline()
     })
